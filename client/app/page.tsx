6f14e9c3468a1c28bae0965463d90861dac14847
Carl Godlewski <cgodlew@gmail.com>
2024-08-09 19:51:04 -0400

    Added logic to handle downloading PDF

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -37,9 +37,29 @@ export default function InvoiceGeneratorApp() {
     
     
     try {
-      const response = await axios.post('', {
+      const response = await axios.post('http://localhost:6006/api/invoice', {
         id: uuidExtract(values.submission_id)
-      })
+      }, {
+		responseType: 'blob' // Important to set this to handle binary data
+	  })
+
+	  const pdfBlob = new Blob([response.data], { type: 'application/pdf' });
+	  const pdfUrl = URL.createObjectURL(pdfBlob);
+
+	  const link = document.createElement('a');
+	  link.href = pdfUrl;
+	  link.setAttribute('download', 'invoice.pdf');
+	  document.body.appendChild(link);
+	  link.click();
+	  
+	  // Clean up the URL object and remove the link element
+	  URL.revokeObjectURL(pdfUrl);
+	  link.remove();
+
+	//   const response = await axios.post('https://garage-backend.onrender.com/getListing', {
+    //     id: uuidExtract(values.submission_id)
+    //   })
+
 
     } catch (error) {
       //TODO: Add Error Message
